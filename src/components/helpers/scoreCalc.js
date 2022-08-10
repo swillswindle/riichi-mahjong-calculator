4f11fe4c4eaaa814947a4c174b268ea37bdab81d
willt <91817538+swillswindle@users.noreply.github.com>
2022-08-09 20:40:59 -0700

    we did it

--- src/components/helpers/scoreCalc.js
+++ src/components/helpers/scoreCalc.js
@@ -4,99 +4,188 @@
 import tiles from "./tiles";
 console.log(tiles.length);
 let simples = [
-  1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 19, 21, 22, 23, 24, 25,
+  1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25,
 ];
 let honors = [27, 28, 29, 30, 31, 32, 33];
 let man = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 let pin = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 let sou = [18, 19, 20, 21, 22, 23, 24, 25, 26];
 let terminals = [0, 8, 9, 17, 18, 26];
-let ones = [0, 9, 18]
-let nines = [8, 17, 26]
+let ones = [0, 9, 18];
+let nines = [8, 17, 26];
 let twos = [1, 10, 19];
 let eights = [7, 16, 25];
+let redFives = [34, 35, 36];
+
+let testhand = [27, 27, 27, 1, 2, 3, 4, 5, 6, 7, 8, 9, 33, 33];
+
+function replaceFives(hand, currentHand) {
+  for (let tile of hand) {
+    switch (tile) {
+      case 34:
+        hand.splice(hand.indexOf(tile), 1);
+        hand.push(4);
+        currentHand.doraCount++;
+        break;
+      case 35:
+        hand.splice(hand.indexOf(tile), 1);
+        hand.push(13);
+        currentHand.doraCount++;
+        break;
+      case 36:
+        hand.splice(hand.indexOf(tile), 1);
+        hand.push(22);
+        currentHand.doraCount++;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
 
 function isHand(hand) {
-  let melds = [[], [], [], [], []];
+  const currentHand = {
+    tiles: [],
+    kanCount: 0,
+    pairCount: 0,
+    ponCount: 0,
+    chiCount: 0,
+    doraCount: 0,
+    melds: [[], [], [], [], []],
+  };
   let currentMeld = 0;
-  let kanCount = 0;
-  let pairCount = 0;
-  let ponCount = 0;
-  let chiCount = 0;
+  replaceFives(hand, currentHand);
+  currentHand.tiles = JSON.parse(JSON.stringify(hand));
+  console.log(hand, currentHand.tiles);
 
-    for (let tile of hand) {
-        if (honors.includes(tile)){
-            hand.splice(hand.indexOf(tile), 1);
-            melds[currentMeld].push(tile);
-            if (currentMeld.length !== 0) {
-                currentMeld++;
-            }
-            switch (hand.filter(handTile => handTile === tile).length) {
-                case 0: 
-                    return false;
-                case 1:
-                    melds[currentMeld].push(tile)
-                    hand = hand.filter(handTile => handTile !== tile);
-                    pairCount++;
-                    currentMeld++;
-                    break;
-                case 2: 
-                    melds[currentMeld].push(tile);
-                    melds[currentMeld].push(tile);
-                    hand = hand.filter(handTile => handTile !== tile);
-                    ponCount++;
-                    currentMeld++;
-                    break;
-                case 3:
-                    melds[currentMeld].push(tile);
-                    melds[currentMeld].push(tile);
-                    melds[currentMeld].push(tile);
-                    hand = hand.filter(handTile => handTile !== tile);
-                    kanCount++;
-                    currentMeld++;
-                    break;
-            }
+  for (let tile of currentHand.tiles) {
+    if (tile === -1) {
+      continue;
+    }
+    if (honors.includes(tile)) {
+      currentHand.tiles.splice(currentHand.tiles.indexOf(tile), 1);
+      currentHand.tiles.unshift(-1);
+      if (currentHand.melds[currentMeld].length !== 0) {
+        currentMeld++;
+      }
+      currentHand.melds[currentMeld].push(tile);
 
-        }
-        else if (simples.includes(tile) || terminals.includes(tile)) {
-            hand.splice(hand.indexOf(tile), 1);
-            if (currentMeld.length !== 0) {
-                currentMeld++;
-            }
-            melds[currentMeld].push(tile);
-            switch (hand.filter(handTile => handTile === tile).length) {
-                case 0: if (!nines.includes(tile) && !eights.includes(tiles) && hand.includes(tile + 1) && hand.includes(tile + 2)) {
-                    melds[currentMeld].push(tile + 1);
-                    melds[currentMeld].push(tile + 2);
-                    hand = hand.filter(handTile => handTile !== tile + 1);
-                    hand = hand.filter(handTile => handTile !== tile + 2);
-                    chiCount++;
-                    currentMeld++;
-                    break;
-                } else if (!ones.includes(tile) && !twos.includes(tile) && hand.includes(tile - 1) && hand.includes(tile - 2)) {
-                    melds[currentMeld].push(tile - 1);
-                    melds[currentMeld].push(tile - 2);
-                    hand = hand.filter(handTile => handTile !== tile - 1);
-                    hand = hand.filter(handTile => handTile !== tile - 2);
-                    chiCount++;
-                    currentMeld++;
-                    break;
-                } else if (!ones.includes(tile) && !nines.includes(tile) && hand.includes(tile -1) && hand.includes(tile + 1)) {
-                    melds[currentMeld].push(tile - 1);
-                    melds[currentMeld].push(tile + 1);
-                    hand = hand.filter(handTile => handTile !== tile - 1);
-                    hand = hand.filter(handTile => handTile !== tile + 1);
-                    chiCount++;
-                    currentMeld++;
-                    break;
-                } else {
-                    return false;
-                }
-                
+      console.log(`line 66: 
+      honor tile: ${tile}
+      currentMeld: ${currentMeld} ${currentHand.melds[currentMeld]}`);
 
-        }
-    }}}
+      switch (
+        currentHand.tiles.filter((handTile) => handTile === tile).length
+      ) {
+        case 0:
+          return false;
+        case 1:
+          currentHand.melds[currentMeld].push(tile);
+          currentHand.tiles.splice(currentHand.tiles.indexOf(tile), 1);
+          currentHand.tiles.unshift(-1);
+          currentHand.pairCount++;
+          currentMeld++;
+          break;
+        case 2:
+          currentHand.melds[currentMeld].push(tile);
+          currentHand.melds[currentMeld].push(tile);
+          currentHand.tiles.splice(currentHand.tiles.indexOf(tile), 1);
+          currentHand.tiles.splice(currentHand.tiles.indexOf(tile), 1);
+          currentHand.tiles.unshift(-1);
+          currentHand.tiles.unshift(-1);
 
+          currentHand.ponCount++;
+          currentMeld++;
+          break;
+        case 3:
+          currentHand.melds[currentMeld].push(tile);
+          currentHand.melds[currentMeld].push(tile);
+          currentHand.melds[currentMeld].push(tile);
+          currentHand.tiles.splice(currentHand.tiles.indexOf(tile), 1);
+          currentHand.tiles.splice(currentHand.tiles.indexOf(tile), 1);
+          currentHand.tiles.splice(currentHand.tiles.indexOf(tile), 1);
+          currentHand.tiles.unshift(-1);
+          currentHand.tiles.unshift(-1);
+          currentHand.tiles.unshift(-1);
+          currentHand.kanCount++;
+          currentMeld++;
+          break;
+      }
+
+      console.log(`line 115: 
+      honor tile: ${tile}
+      currentMeld: ${currentMeld} ${currentHand.melds[currentMeld]}
+      all melds: ${currentHand.melds}
+      pairCount: ${currentHand.pairCount}
+      ponCount: ${currentHand.ponCount}
+      kanCount: ${currentHand.kanCount}
+      chiCount: ${currentHand.chiCount}
+      doraCount: ${currentHand.doraCount}`);
+    } else if (simples.includes(tile) || terminals.includes(tile)) {
+      currentHand.tiles.splice(currentHand.tiles.indexOf(tile), 1);
+        currentHand.tiles.unshift(-1);
+      if (currentHand.melds[currentMeld].length !== 0) {
+        currentMeld++;
+      }
+      currentHand.melds[currentMeld].push(tile);
+
+      switch (
+        currentHand.tiles.filter((handTile) => handTile === tile).length
+      ) {
+        case 0:
+          if (
+            !nines.includes(tile) &&
+            !eights.includes(tile) &&
+            currentHand.tiles.includes(tile + 1) &&
+            currentHand.tiles.includes(tile + 2)
+          ) {
+            currentHand.melds[currentMeld].push(tile + 1);
+            currentHand.melds[currentMeld].push(tile + 2);
+            currentHand.tiles.splice(currentHand.tiles.indexOf(tile + 1), 1);
+            currentHand.tiles.splice(currentHand.tiles.indexOf(tile + 2), 1);
+            currentHand.tiles.unshift(-1);
+            currentHand.tiles.unshift(-1);
+            currentHand.chiCount++;
+            currentMeld++;
+            break;
+          } else if (
+            !ones.includes(tile) &&
+            !twos.includes(tile) &&
+            currentHand.tiles.includes(tile - 1) &&
+            currentHand.tiles.includes(tile - 2)
+          ) {
+            currentHand.melds[currentMeld].push(tile - 1);
+            currentHand.melds[currentMeld].push(tile - 2);
+            currentHand.tiles.splice(currentHand.tiles.indexOf(tile - 1), 1);
+            currentHand.tiles.splice(currentHand.tiles.indexOf(tile - 2), 1);
+            currentHand.tiles.unshift(-1);
+            currentHand.tiles.unshift(-1);
+            currentHand.chiCount++;
+            currentMeld++;
+            break;
+          } else if (
+            !ones.includes(tile) &&
+            !nines.includes(tile) &&
+            currentHand.tiles.includes(tile - 1) &&
+            currentHand.tiles.includes(tile + 1)
+          ) {
+            currentHand.melds[currentMeld].push(tile - 1);
+            currentHand.melds[currentMeld].push(tile + 1);
+            currentHand.tiles.splice(currentHand.tiles.indexOf(tile - 1), 1);
+            currentHand.tiles.splice(currentHand.tiles.indexOf(tile + 1), 1);
+            currentHand.tiles.unshift(-1);
+            currentHand.tiles.unshift(-1);
+            currentHand.chiCount++;
+            currentMeld++;
+            break;
+          } else {
+            return false;
+          }
+      }
+    }
+  }
+}
 function isOrphans(hand) {
   for (let tile of hand) {
     if (simples.includes(tile)) {
@@ -106,7 +195,6 @@ function isOrphans(hand) {
   return new Set(hand).size === 13;
 }
 
-let testhand = [0, 8, 9, 17, 18, 26, 27, 28, 29, 30, 31, 32, 33, 33];
 let badhand = [0, 8, 9, 17, 18, 26, 27, 28, 29, 30, 31, 32, 0, 8];
 
 class Yaku {
@@ -139,3 +227,5 @@ class AllTerminals extends Yaku {
 
 let test = new AllTerminals();
 test.execute();
+
+isHand(testhand);
